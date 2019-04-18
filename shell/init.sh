cfb2015e796739240475d47cb55df499a2092d7b
peromage <peromage@outlook.com>
2019-04-18 00:40:15 -0400

    shell: Updated init.sh

--- shell/init.sh
+++ shell/init.sh
@@ -1,10 +1,10 @@
 # interactive mode initialization script
-## prevent from being loaded twice
-#if [ -z "$_INIT_SH_LOADED" ]; then
-#    _INIT_SH_LOADED=1
-#else
-#    return
-#fi
+# prevent from being loaded twice
+if [ -z "$_INIT_SH_LOADED" ]; then
+    _INIT_SH_LOADED=1
+else
+    return
+fi
 
 # if in non-interactive mode then exit
 case "$-" in
@@ -13,26 +13,44 @@ case "$-" in
 esac
 
 # ============== functions ===========================
-function _prompt() {
-    ext=$?
-    ret_code=""
-    user_status=""
+function _bash_prompt() {
+    # backup last exit code
+    pret=$?
 
-    case "$ext" in
-        0) ret_code="\[\e[32m\]:)\[\e[m\]" ;;
-        *) ret_code="\[\e[31m\]:(\[\e[m\]" ;;
+    # print word in color
+    # usage: pwic <word> <fgcolor>
+    function pwic() {
+        echo "\[\e[${2}m\]${1}\[\e[m\]"
+    }
+
+    # variables
+    pemoji=""
+    psymbol=""
+    ppath="$(pwic "\w" "34")"
+    ptime="$(date +"%H:%M")"
+    pinfo="\u@\h"
+
+    case "$pret" in
+        0) pemoji="$(pwic ":)" "32")" ;;
+        *) pemoji="$(pwic ":(" "31")" ;;
     esac
 
     case "$UID" in
-        0) user_status="\[\e[31m\]#\[\e[m\]" ;;
-        *) user_status="\[\e[32m\]\$\[\e[m\]" ;;
+        0)
+            psymbol="#"
+            pinfo="$(pwic $pinfo 31)"
+            ;;
+        *)
+            psymbol="\$"
+            pinfo="$(pwic $pinfo 32)"
+            ;;
     esac
 
-    PS1="$ret_code \[\e[34m\]\w\[\e[m\] $user_status "
+    PS1="${pinfo}:${ppath} ${pemoji} ${psymbol} "
 }
 # ====================================================
 if [ -n "$BASH_VERSION" ]; then
-    export PROMPT_COMMAND=_prompt
+    export PROMPT_COMMAND=_bash_prompt
 #elif [ -n "$ZSH_VERSION"]; then
 
 fi
