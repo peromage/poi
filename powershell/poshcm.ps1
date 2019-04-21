64c208f666f7c5841af3d1f2f137bcdbf0b5a953
peromage <peromage@outlook.com>
2019-04-21 11:02:54 -0400

    powershell: added colortest

--- powershell/poshcm.ps1
+++ powershell/poshcm.ps1
@@ -46,9 +46,41 @@ Set-PSReadlineOption -Color @{
 
 function Show-ConsoleColor {
     $colors = [enum]::GetValues([System.ConsoleColor])
-    Foreach ($bgcolor in $colors){
-        Foreach ($fgcolor in $colors) { Write-Host "$fgcolor|"  -ForegroundColor $fgcolor -BackgroundColor $bgcolor -NoNewLine }
+    foreach ($bgcolor in $colors){
+        foreach ($fgcolor in $colors) { Write-Host "$fgcolor|"  -ForegroundColor $fgcolor -BackgroundColor $bgcolor -NoNewLine }
         Write-Host " on $bgcolor"
+    }
+}
+
+function Show-ColorTest {
+    $width = 7
+    $word = "gYm"
+    $esc = [char]0x1b
+
+    # print columns with background colors
+    # usage: pcbg <word> <color_code>
+    function pcbg {
+        param($word, $colorcode)
+        Write-Host ("{0,$width}" -f $colorcode.Substring(2)) -NoNewline
+        Write-Host ("{0,$width}" -f $word) -NoNewline
+        foreach ($i in 40..47) {
+            Write-Host ("$colorcode$esc[${i}m{0,$width}$esc[0m" -f $word) -NoNewline
+        }
+        Write-Host
+    }
+
+    # header
+    Write-Host ("{0,$width} {1,$width}" -f "code", "m") -NoNewline
+    foreach ($i in 40..47) {
+        Write-Host ("{0,$width}" -f "${i}m") -NoNewline
+    }
+    Write-Host
+    # print row by row
+    pcbg $word "$esc[0m"
+    pcbg $word "$esc[1m"
+    foreach ($i in 30..37) {
+        pcbg $word "$esc[${i}m"
+        pcbg $word "$esc[1;${i}m"
     }
 }
 
@@ -86,7 +118,7 @@ function Prompt {
         Write-Host "${login}:${currpath} ${face} $("$symbol" * ($nestedPromptLevel + 1))" -NoNewline
     }
 
-    if(Test-Administrator) {
+    if (Test-Administrator) {
         style1 "#" "31m"
     }
     else {
