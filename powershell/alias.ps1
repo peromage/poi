54c5a1ca835a3dca621001f68b3a13b7aaa2a92a
peromage <peromage@outlook.com>
2019-05-08 12:20:00 -0400

    powershell: updated gitac

--- powershell/alias.ps1
+++ powershell/alias.ps1
@@ -1,3 +1,3 @@
 function gitss { git status }
 function gitgg { git log --graph --pretty=format:'%Cred%h%Creset [%an] - %s %Cgreen(%ci)%Creset %C(cyan)%d%Creset' --abbrev-commit --date=relative }
-function gitac($msg) { ($msg.length -ne 0) -and $(git add -A; git commit -m $msg) }
+function gitac($msg) { if($msg.length -ne 0){git add -A; git commit -m $msg} else{Write-Output "Usage: gitac <message>"} }
