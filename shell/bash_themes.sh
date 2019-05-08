69be20f40c1bc6a05e0b860ed7d306f8ea691c3e
peromage <peromage@outlook.com>
2019-05-07 21:19:35 -0400

    shell: added fish_collapsed_pwd

--- shell/bash_themes.sh
+++ shell/bash_themes.sh
@@ -1,15 +1,15 @@
 # Control Sequence Introducer
 # usage: _csi_ <word> <csi_code>
-_CSI_() {
+function _CSI_() {
     echo "\[\e[${2}\]${1}\[\e[0m\]"
 }
 
-_RET_() {
+function _RET_() {
     local ret=$?
     echo "$ret"
 }
 
-_RET_FACE_() {
+function _RET_FACE_() {
     local ret=$?
     if [ "$ret" -ne 0 ]; then
         echo "$(_CSI_ ":(" "31m")"
@@ -19,7 +19,7 @@ _RET_FACE_() {
 }
 
 # usage: _bash_style1_ <$UID>
-_BASH_THEME1_() {
+function _BASH_THEME1_() {
     case "$1" in
         0) export PS1="[$(_CSI_ "\u@\h" "31m"):\W]# " ;;
         *) export PS1="[$(_CSI_ "\u@\h" "32m"):\W]$ " ;;
@@ -27,7 +27,7 @@ _BASH_THEME1_() {
 }
 
 # usage: _bash_style1_ <$UID>
-_BASH_THEME2_() {
+function _BASH_THEME2_() {
     case "$1" in
         0) export PS1="[$(_CSI_ "\u@\h" "31m"):\W \[\$?\]]# " ;;
         *) export PS1="[$(_CSI_ "\u@\h" "32m"):\W \[\$?\]]$ " ;;
@@ -35,7 +35,7 @@ _BASH_THEME2_() {
 }
 
 # usage: _bash_style1_ <$UID>
-_BASH_THEME3_() {
+function _BASH_THEME3_() {
     case "$1" in
         0) export PS1="$(_CSI_ "\u@\h" "31m") \w $(_RET_FACE_)\n+++ " ;;
         *) export PS1="$(_CSI_ "\u@\h" "32m") \w $(_RET_FACE_)\n--- " ;;
