6a73cb73fe47cdd22af8e4d270f94e0f76232145
peromage <peromage@outlook.com>
2019-05-29 17:00:07 -0400

    powershell: updated theme selector

--- powershell/themes.psm1
+++ powershell/themes.psm1
@@ -26,34 +26,38 @@ $CONSOLE_PALETTE = [ordered]@{
     "47m" = [System.ConsoleColor]::Gray
 }
 
-
-
-function _COLORTOOL_ {
-    param($theme)
+function SET_SCHEME {
+    param($ColorToolArgs, $PSReadlineArgs)
 
     $COLORTOOL = "$PSScriptRoot\colortool\ColorTool.exe"
+    $DEFAULTCOLORS = @{
+        "CommandColor" = "$([char]0x1b)[93m"
+        "CommentColor" = "$([char]0x1b)[32m"
+        "ContinuationPromptColor" = "$([char]0x1b)[37m"
+        "DefaultTokenColor" = "$([char]0x1b)[37m"
+        "EmphasisColor" = "$([char]0x1b)[96m"
+        "ErrorColor" = "$([char]0x1b)[91m"
+        "KeywordColor" = "$([char]0x1b)[92m"
+        "MemberColor" = "$([char]0x1b)[97m"
+        "NumberColor" = "$([char]0x1b)[97m"
+        "OperatorColor" = "$([char]0x1b)[90m"
+        "ParameterColor" = "$([char]0x1b)[90m"
+        "SelectionColor" = "$([char]0x1b)[30;47m"
+        "StringColor" = "$([char]0x1b)[36m"
+        "TypeColor" = "$([char]0x1b)[37m"
+        "VariableColor" = "$([char]0x1b)[92m"
+    }
+
+    # only apply scheme when colortool exists
     if (Test-Path $COLORTOOL) {
+        if ($ColorToolArgs) { Invoke-Expression "$COLORTOOL -q $ColorToolArgs" }
+        # apply psreadline 
         [System.Console]::ResetColor()
-        Invoke-Expression $COLORTOOL + " $theme"
+        if ($PSReadlineArgs) { Set-PSReadlineOption -colors $PSReadlineArgs }
+        else { Set-PSReadlineOption -colors $DEFAULTCOLORS }
         return 0
     }
     return -1
-}
-
-function _THEME_DRACULA_ {
-    if ((_COLORTOOL_ "Dracula-Alt.itermcolors") -eq 0) {
-        # set psreadline
-        Set-PSReadlineOption -Color @{
-            "Command" = [ConsoleColor]::Green
-            "Parameter" = [ConsoleColor]::Gray
-            "Operator" = [ConsoleColor]::Magenta
-            "Variable" = [ConsoleColor]::White
-            "String" = [ConsoleColor]::Yellow
-            "Number" = [ConsoleColor]::Blue
-            "Type" = [ConsoleColor]::Cyan
-            "Comment" = [ConsoleColor]::DarkCyan
-        }
-    }
 }
 
 function THEME_SELECTOR {
@@ -61,8 +65,29 @@ function THEME_SELECTOR {
 
     # make a choice
     switch ($Scheme) {
-        "dracula-alt" { _THEME_DRACULA_; break }
+        "dracula" {
+            $colors = @{
+                "Command" = [ConsoleColor]::Green
+                "Parameter" = [ConsoleColor]::Gray
+                "Operator" = [ConsoleColor]::Magenta
+                "Variable" = [ConsoleColor]::White
+                "String" = [ConsoleColor]::Yellow
+                "Number" = [ConsoleColor]::Blue
+                "Type" = [ConsoleColor]::Cxyan
+                "Comment" = [ConsoleColor]::DarkCyan
+            }
+            SET_SCHEME "Dracula-Alt.itermcolors" $colors
+            break
+        }
+        "solarized-dark" { SET_SCHEME "solarized_dark.itermcolors"; break }
+        "solarized-light" { SET_SCHEME "solarized_light.itermcolors"; break }
+        "campbell" { SET_SCHEME "campbell.ini"; break }
+        "campbell-legacy" { SET_SCHEME "campbell-legacy.ini"; break }
+        "cmd-legacy" { SET_SCHEME "cmd-legacy.ini"; break }
+        "onehalf-dark" { SET_SCHEME "OneHalfDark.itermcolors"; break }
+        "onehalf-light" { SET_SCHEME "OneHalfLight.itermcolors"; break }
+        "deuteranopia" { SET_SCHEME "deuteranopia.itermcolors"; break }
     }
 }
 
-Export-ModuleMember -Function THEME_SELECTOR -Variable CONSOLE_PALETTE
+Export-ModuleMember -Function THEME_SELECTOR, SET_SCHEME -Variable CONSOLE_PALETTE
