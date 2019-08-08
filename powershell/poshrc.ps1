6efb5bf419e269c2cbe175469f5b15d410914c53
peromage <peromage@outlook.com>
2019-08-07 23:51:39 -0400

    powershell: added shim consoleapp source code

--- powershell/poshrc.ps1
+++ powershell/poshrc.ps1
@@ -9,7 +9,7 @@ if (Test-Path "$PSScriptRoot\alias.psm1") { Import-Module "$PSScriptRoot\alias.p
 
 if (Test-Path "$PSScriptRoot\utils.psm1") { Import-Module "$PSScriptRoot\utils.psm1" }
 
-if (Test-Path "$PSScriptRoot\shims.psm1") { Import-Module "$PSScriptRoot\shims.psm1" }
+if (Test-Path "$PSScriptRoot\shim.psm1") { Import-Module "$PSScriptRoot\shim.psm1" }
 
 if (Test-Path "$PSScriptRoot\themes.psm1") { 
     Import-Module "$PSScriptRoot\themes.psm1"
