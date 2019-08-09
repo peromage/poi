00de03ae26132e1323d1cc3ab731800ea78a18fe
peromage <peromage@outlook.com>
2019-08-09 13:21:10 -0400

    powershell: Replaced default hash table to json format

--- powershell/constants.psm1
+++ powershell/constants.psm1
@@ -26,78 +26,7 @@ $CONSOLE_PALETTE = [ordered]@{
     "47m" = [System.ConsoleColor]::Gray
 }
 
-# Default PSReadline color scheme
-$DEFAULT_PSCOLORS = [ordered]@{
-    "Command" = "$([char]0x1b)[93m"
-    "Comment" = "$([char]0x1b)[32m"
-    "ContinuationPrompt" = "$([char]0x1b)[37m"
-    "Default" = "$([char]0x1b)[37m"
-    "Emphasis" = "$([char]0x1b)[96m"
-    "Error" = "$([char]0x1b)[91m"
-    "Keyword" = "$([char]0x1b)[92m"
-    "Member" = "$([char]0x1b)[97m"
-    "Number" = "$([char]0x1b)[97m"
-    "Operator" = "$([char]0x1b)[90m"
-    "Parameter" = "$([char]0x1b)[90m"
-    "Selection" = "$([char]0x1b)[30;47m"
-    "String" = "$([char]0x1b)[36m"
-    "Type" = "$([char]0x1b)[37m"
-    "Variable" = "$([char]0x1b)[92m"
-}
-
-# Default color theme (ini format)
-$DEFAULT_SCHEME = @"
-[table]
-DARK_BLACK = 38,40,53
-DARK_BLUE = 188,146,248
-DARK_GREEN = 79,249,123
-DARK_CYAN = 97,114,164
-DARK_RED = 255,84,84
-DARK_MAGENTA = 255,121,197
-DARK_YELLOW = 255,183,108
-DARK_WHITE = 247,247,241
-BRIGHT_BLACK = 84,84,84
-BRIGHT_BLUE = 188,146,248
-BRIGHT_GREEN = 79,249,123
-BRIGHT_CYAN = 138,232,252
-BRIGHT_RED = 255,84,84
-BRIGHT_MAGENTA = 255,121,197
-BRIGHT_YELLOW = 240,249,139
-BRIGHT_WHITE = 255,255,255
-
-[screen]
-FOREGROUND = DARK_WHITE
-BACKGROUND = DARK_BLACK
-
-[popup]
-FOREGROUND = DARK_CYAN
-BACKGROUND = BRIGHT_WHITE
-"@
-
-$MyBin = @{
-    "7z" = "7-zip\7zFM.exe"
-    "aria2" = "aria2\aria2c.exe"
-    "git" = "git\bin\git.exe"
-    "bash" = "git\bin\bash.exe"
-    "jcp" = "jcpicker\jcpicker.exe"
-    "picpick" = "picpick\picpick.exe"
-    "vnc" = "vncviewer\vncviewer.exe"
-    "tc" = "totalcommander\TOTALCMD64.EXE"
-    "code" = "vscode\bin\code.cmd"
-    "vlc" = "vlc\vlc.exe"
-    "chrome" = "chrome\chrome.exe"
-    "npp" = "notepad++\notepad++.exe"
-}
-
-# Default style
-function DEFAULT_PROMPT {
-    return "PS $pwd> "
-}
-
 # Escape sequence
 $ESC = [char]0x1b
 
-# Local save dir
-$SAVEDIR = Join-Path $PSScriptRoot "..\.save\"
-
-Export-ModuleMember -Variable * -Function *
+Export-ModuleMember -Variable *
---
+++ powershell/defaults.psm1
@@ -0,0 +1,76 @@
+# Default PSReadline color scheme
+$DEFAULT_PSCOLORS = @"
+{
+    "Command":  "\u001b[93m",
+    "Comment":  "\u001b[32m",
+    "ContinuationPrompt":  "\u001b[37m",
+    "Default":  "\u001b[37m",
+    "Emphasis":  "\u001b[96m",
+    "Error":  "\u001b[91m",
+    "Keyword":  "\u001b[92m",
+    "Member":  "\u001b[97m",
+    "Number":  "\u001b[97m",
+    "Operator":  "\u001b[90m",
+    "Parameter":  "\u001b[90m",
+    "Selection":  "\u001b[30;47m",
+    "String":  "\u001b[36m",
+    "Type":  "\u001b[37m",
+    "Variable":  "\u001b[92m"
+}
+"@
+
+# Default color theme (ini format)
+$DEFAULT_SCHEME = @"
+[table]
+DARK_BLACK = 38,40,53
+DARK_BLUE = 188,146,248
+DARK_GREEN = 79,249,123
+DARK_CYAN = 97,114,164
+DARK_RED = 255,84,84
+DARK_MAGENTA = 255,121,197
+DARK_YELLOW = 255,183,108
+DARK_WHITE = 247,247,241
+BRIGHT_BLACK = 84,84,84
+BRIGHT_BLUE = 188,146,248
+BRIGHT_GREEN = 79,249,123
+BRIGHT_CYAN = 138,232,252
+BRIGHT_RED = 255,84,84
+BRIGHT_MAGENTA = 255,121,197
+BRIGHT_YELLOW = 240,249,139
+BRIGHT_WHITE = 255,255,255
+
+[screen]
+FOREGROUND = DARK_WHITE
+BACKGROUND = DARK_BLACK
+
+[popup]
+FOREGROUND = DARK_CYAN
+BACKGROUND = BRIGHT_WHITE
+"@
+
+$MyBin = @"
+{
+    "tc":  "totalcommander\\TOTALCMD64.EXE",
+    "chrome":  "chrome\\chrome.exe",
+    "jcp":  "jcpicker\\jcpicker.exe",
+    "picpick":  "picpick\\picpick.exe",
+    "aria2":  "aria2\\aria2c.exe",
+    "bash":  "git\\bin\\bash.exe",
+    "7z":  "7-zip\\7zFM.exe",
+    "git":  "git\\bin\\git.exe",
+    "vlc":  "vlc\\vlc.exe",
+    "code":  "vscode\\bin\\code.cmd",
+    "npp":  "notepad++\\notepad++.exe",
+    "vnc":  "vncviewer\\vncviewer.exe"
+}
+"@
+
+# Default style
+function DEFAULT_PROMPT {
+    return "PS $pwd> "
+}
+
+# Local save dir
+$SAVEDIR = Join-Path $PSScriptRoot "..\.save\"
+
+Export-ModuleMember -Variable * -Function *
