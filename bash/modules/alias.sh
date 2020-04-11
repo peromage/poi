3383ac10b7399482766507cec6c1fc3e5ac4c0f6
peromage <peromage@outlook.com>
2020-04-11 10:20:04 -0400

    Bash: Update alias

--- bash/modules/alias.sh
+++ bash/modules/alias.sh
@@ -1,1 +1,6 @@
 alias ll="ls -la --color=always --group-directories-first"
+alias gst="git status"
+alias glo="git log --graph --pretty=format:'%Cred%h%Creset [%an] - %s %Cgreen(%ci)%Creset %C(cyan)%d%Creset' --abbrev-commit --date=relative"
+alias gac="git add -A; git commit -m"
+alias gch="git checkout"
+alias gbr="git branch"
