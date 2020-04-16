451516de12bc321ae04f849233d46917f4ff3e00
peromage <peromage@outlook.com>
2020-04-16 14:31:01 -0400

    PowerShell: Add sort options for ll command

--- posh/modules/commands.psm1
+++ posh/modules/commands.psm1
@@ -1,8 +1,44 @@
 # Common functions
 function Show-Dir {
-    Get-ChildItem $args[0] `
-    | Select-Object Mode,Length,LastWriteTime,Name,Target `
-    | Format-Table -AutoSize
+    [CmdletBinding(PositionalBinding=$false, DefaultParameterSetName="sortByDefault")]
+    param(
+        [Parameter(Position=0)][string]$path=$PWD.Path,
+        [Parameter(Mandatory=$false, ParameterSetName="sortByWriteTime")][switch]$write,
+        [Parameter(Mandatory=$false, ParameterSetName="sortByAccessTime")][switch]$access,
+        [Parameter(Mandatory=$false, ParameterSetName="sortByCreationTime")][switch]$creation,
+        [Parameter(Mandatory=$false, ParameterSetName="sortByName")][switch]$name,
+        [Parameter(Mandatory=$false, ParameterSetName="sortBySize")][switch]$size
+    )
+    if ($write) {
+        Get-ChildItem $path `
+        | Sort-Object LastWriteTime `
+        | Select-Object Mode,Length,LastWriteTime,Name,Target `
+        | Format-Table -AutoSize
+    } elseif ($access) {
+        Get-ChildItem $path `
+        | Sort-Object LastAccessTime `
+        | Select-Object Mode,Length,LastAccessTime,Name,Target `
+        | Format-Table -AutoSize
+    } elseif ($creation) {
+        Get-ChildItem $path `
+        | Sort-Object CreationTime `
+        | Select-Object Mode,Length,CreationTime,Name,Target `
+        | Format-Table -AutoSize
+    } elseif ($name) {
+        Get-ChildItem $path `
+        | Sort-Object Name `
+        | Select-Object Mode,Length,LastWriteTime,Name,Target `
+        | Format-Table -AutoSize
+    } elseif ($size) {
+        Get-ChildItem $path `
+        | Sort-Object Length `
+        | Select-Object Mode,Length,LastWriteTime,Name,Target `
+        | Format-Table -AutoSize
+    } else {
+        Get-ChildItem $path `
+        | Select-Object Mode,Length,LastWriteTime,Name,Target `
+        | Format-Table -AutoSize
+    }
 }
 
 function Test-Admin {
