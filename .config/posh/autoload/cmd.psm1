a17bf8c998338cf5d5508cfe67d1ff9ede848c68
peromage <peromage@outlook.com>
2020-05-01 14:34:58 -0400

    PowerShell: Add autoload mechanism

--- .config/posh/autoload/cmd.psm1
+++ .config/posh/autoload/cmd.psm1
@@ -81,39 +81,11 @@ function Switch-Admin {
     }
 }
 
-# Git functions
-function Git-Status {
-    git status
-}
-
-function Git-Log {
-    git log --graph --pretty=format:'%Cred%h%Creset [%an] - %s %Cgreen(%ci)%Creset %C(cyan)%d%Creset' --abbrev-commit --date=relative
-}
-
-function Git-AddCommit {
-    git add -A
-    git commit -m $args[0]
-}
-
-function Git-Branch {
-    git branch $args
-}
-
-function Git-Checkout {
-    git checkout $args
-}
-
-
 # Alias
 Set-Alias -Name ll -Value Show-Dir
 Set-Alias -Name issu -Value Test-Admin
 Set-Alias -Name sudo -Value Grant-Admin
 Set-Alias -Name su -Value Switch-Admin
-Set-Alias -Name gst -Value Git-Status
-Set-Alias -Name glo -Value Git-Log
-Set-Alias -Name gac -Value Git-AddCommit
-Set-Alias -Name gch -Value Git-Checkout
-Set-Alias -Name gbr -Value Git-Branch
 
 # Exporting
 Export-ModuleMember -Function * -Alias *
--- .config/posh/poshrc.ps1
+++ .config/posh/poshrc.ps1
@@ -1,9 +1,10 @@
 # Author: Fang
 # Date: 2020/02/17
+param([string]$prompt_style="myposh")
 
 # Load core module
 Import-Module -DisableNameChecking "$PSScriptRoot\rcore.psm1"
 # Loading modules
-loadmodule "$PSScriptRoot\modules" commands,shim
+loadmodule "$PSScriptRoot\autoload" *
 # Loading prompt
-loadmodule "$PSScriptRoot\prompts" myposh
+loadmodule "$PSScriptRoot\prompts" $prompt_style
