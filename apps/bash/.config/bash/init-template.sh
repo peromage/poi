45d669d9a5ef12128bdb406f9e641f5a8e6e175b
peromage <peromage@outlook.com>
2020-06-05 00:18:53 -0400

    Bash: Fix a bug in init.sh

--- apps/bash/.config/bash/init-template.sh
+++ apps/bash/.config/bash/init-template.sh
@@ -1,4 +1,4 @@
 # Quick edit configuration file
-RcConfig() { ([ ! -z $EDITOR ] && eval "$EDITOR $BASH_SOURCE") || vi $BASH_SOURCE }
+RcConfig() { ([ ! -z $EDITOR ] && eval "$EDITOR $BASH_SOURCE") || vi $BASH_SOURCE; }
 # Load core file
 eval ". $(dirname $(realpath $BASH_SOURCE))/bashrc.sh"
