a8a02b9a0f1208c9fde28da461b771562b54a3c3
peromage <peromage@outlook.com>
2021-02-24 23:20:34 -0500

    Add order for pwsh rc scripts

--- ripwsh/ripwsh.psm1
+++ ripwsh/ripwsh.psm1
@@ -55,14 +55,14 @@ Helper functions
 ------------------------------------------------------------------------------#>
 # This is faster than the trivial sourcing
 # In order to source the script in current scope, source the returned scriptblock
-function source_script {
+function ri_source_script {
     param([string]$rel_path)
     $abs_path = Join-Path $ri_meta.Home $rel_path
     return [scriptblock]::Create([System.IO.File]::ReadAllText($abs_path, [System.Text.Encoding]::UTF8))
 }
 
 # Just normalize the given path. No file checks
-function normalize_path {
+function ri_normalize_path {
     param([string]$path)
     return $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($path)
 }
@@ -71,19 +71,19 @@ function normalize_path {
 Initialization
 ------------------------------------------------------------------------------#>
 # Sourcing base scripts
-. (source_script base/common.ps1)
+. (ri_source_script base/10_common.ps1)
 if ($IsWindows) {
-    . (source_script base/windows.ps1)
+    . (ri_source_script base/11_windows.ps1)
 }
 
 # Load additional plugins
-$ri_config.plugins | ForEach-Object { . (source_script plugins/$_.ps1) }
+$ri_config.plugins | ForEach-Object { . (ri_source_script plugins/$_.ps1) }
 
 # Load theme
-if (-not ([string]::IsNullOrWhiteSpace($ri_config.theme))) { . (source_script themes/$($ri_config.theme).ps1) }
+if (-not ([string]::IsNullOrWhiteSpace($ri_config.theme))) { . (ri_source_script themes/$($ri_config.theme).ps1) }
 
 # Add script path
-$pathToBeAdded = normalize_path "$PSScriptRoot/scripts"
+$pathToBeAdded = ri_normalize_path "$PSScriptRoot/scripts"
 if (-not ($ENV:Path -match [regex]::Escape($pathToBeAdded))) {
     $ENV:Path += $ri_meta.PathSeparator + $pathToBeAdded
 }
