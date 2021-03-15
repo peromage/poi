20bd8459e25c60844205687a9c579538d3520ecf
peromage <peromage@outlook.com>
2021-03-14 22:06:06 -0400

    Ripwsh: Update init.ps1

--- init.ps1
+++ init.ps1
@@ -1,33 +1,39 @@
-<###############################################################################
-
-.SYNOPSIS
-This file should be sourced to initialize rice configs.
-
-Created by peromage 2021/02/24
-Last modified 2021/03/14
-
-###############################################################################>
-
-#===============================================================================
+################################################################################
+#
+# .SYNOPSIS
+# Pwsh initialization template file
+#
+# Created by peromage 2021/02/24
+# Last modified 2021/03/14
+#
+#
+# This section should remain untouched
 # Initialization
-# NOTE: Replace $init_home to the path of rice repo if this file is not at
-# the root of rice
 $init_home = $PSScriptRoot
 $init_file = $MyInvocation.MyCommand.Source
 $ENV:PSModulePath += [IO.Path]::PathSeparator + $init_home
-#-------------------------------------------------------------------------------
+#
+# Examples of configuration outside of ripwsh
+#
+# z.lua
+#------
+#iex ((lua54 $init_home\bin\z.lua --init powershell enhanced once) -join "`n")
+#
 Import-Module ripwsh -ArgumentList @{
-
+#
+# Examples of configuration options of ripwsh
+#
 # Prompt theme
 #-------------
-theme = "my_lite"
-
+#theme = "my_lite"
+#
 # A list of string for mods under ripwsh/mods
 #--------------------------------------------------
-mods = @('basic', 'nav', 'windows', 'env')
+#mods = @('basic', 'nav', 'windows', 'env')
+#
+################################################################################
 
+
+### Rice config should start before this line ###
 }
-#===============================================================================
-
-# z.lua
-#iex ((lua54 $init_home\bin\z.lua --init powershell enhanced once) -join "`n")
+### Other config should start after this line ###
