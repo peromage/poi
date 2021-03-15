6fa11bd8994daea764c06fe1cdf48d7817ffcc3a
peromage <peromage@outlook.com>
2021-03-14 22:11:59 -0400

    Ribash: Update init.sh

--- init.sh
+++ init.sh
@@ -6,29 +6,32 @@
 # Created by peromage 2021/02/24
 # Last modified 2021/03/14
 #
-################################################################################
-
-#===============================================================================
-[ -z "$BASH_VERSION" ] && return
-#-------------------------------------------------------------------------------
+#
+# This section should remain untouched
 # Initialization
-# NOTE: Redirect init_home to the path of rice repo if this file is not at
-# the root of rice
+[ -z "$BASH_VERSION" ] && return
 init_home=$(dirname $(realpath "$BASH_SOURCE"))
 init_file=$(realpath "$BASH_SOURCE")
-#-------------------------------------------------------------------------------
-
+#
+# Examples of configuration outside of ribash
+#
+# z.lua
+#------
+#eval "$(lua5.3 $init_home/bin/z.lua --init bash enhanced once)"
+#
+# Examples of configuration options of ribash
+#
 # Prompt theme
 #-------------
-rice_theme="my_bash"
-
+#rice_theme="my_bash"
+#
 # Space separated string for mods under ribash/mods
 #--------------------------------------------------------
-rice_mods="basic nav env"
+#rice_mods="basic nav env"
+#
+################################################################################
 
-#-------------------------------------------------------------------------------
+
+### Rice config should start before this line ###
 . $init_home/ribash/ribash.sh
-#===============================================================================
-
-# z.lua
-#eval "$(lua54 $init_home/bin/z.lua --init bash enhanced once)"
+### Other config should start after this line ###
